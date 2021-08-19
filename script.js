1871141f46c57109ac80ac3f768a3ef3718643eb
Leonardo Henrique de Azevedo <leonardohenriquedeazevedo@gmail.com>
2021-08-19 18:03:54 -0300

    write

--- script.js
+++ script.js
@@ -10,10 +10,11 @@ const menuMobile = () => {
 
 menu.addEventListener('click', menuMobile);
 
-// Close Mobile Menu when clicking on a menu item
+// Fecha o menu quando clicar na logo ou em algum item dentro dele
 const hideMobileMenu = () => {
   const menuBars = document.querySelector('.esta-ativado');
-  if (window.innerWidth <= 768 && menuBars) {
+  // window.innerWidth <= 768 && 
+  if (menuBars) {
     menu.classList.toggle('esta-ativado');
     menuNavegacao.classList.remove('ativado');
   };
@@ -22,36 +23,49 @@ const hideMobileMenu = () => {
 menuNavegacao.addEventListener('click', hideMobileMenu);
 logoMenu.addEventListener('click', hideMobileMenu);
 
+// Efeito de digitar no título
+const tituloAreaInicial = document.querySelector("h2");
+
+function efeitoEscrever(el) {
+  const textoNoArray = el.innerHTML.split("");
+  el.innerHTML = '';
+  textoNoArray.forEach((letra, index) => {
+    setTimeout(() => el.innerHTML += letra, 75 * index);
+  });
+}
+
+efeitoEscrever(tituloAreaInicial);
+
 // Serve para ficar mostrando o menu seguindo a pessoa enquanto está ativado
-const highlightMenu = () => {
-  const element = document.querySelector('.highlight');
-  const homeMenu = document.querySelector('#home-page');
-  const aboutMenu = document.querySelector('#about-page');
-  const servicesMenu = document.querySelector('#services-page');
-  let scrollPosition = window.scrollY;
+// const highlightMenu = () => {
+//   const element = document.querySelector('.highlight');
+//   const homeMenu = document.querySelector('#home-page');
+//   const aboutMenu = document.querySelector('#about-page');
+//   const servicesMenu = document.querySelector('#services-page');
+//   let scrollPosition = window.scrollY;
 
-  // Adds 'highlight' class to menu items
-  if (window.innerWidth > 960 && scrollPosition < 600) {
-    homeMenu.classList.add('highlight');
-    aboutMenu.classList.remove('highlight');
-    servicesMenu.classList.remove('highlight');
-    return;
-  } else if (window.innerWidth > 960 && scrollPosition < 1400) {
-    aboutMenu.classList.add('highlight');
-    homeMenu.classList.remove('highlight');
-    servicesMenu.classList.remove('highlight');
-    return;
-  } else if (window.innerWidth > 960 && scrollPosition < 2345) {
-    servicesMenu.classList.add('highlight');
-    aboutMenu.classList.remove('highlight');
-    homeMenu.classList.remove('highlight');
-    return;
-  };
+//   // Adds 'highlight' class to menu items
+//   if (window.innerWidth > 960 && scrollPosition < 600) {
+//     homeMenu.classList.add('highlight');
+//     aboutMenu.classList.remove('highlight');
+//     servicesMenu.classList.remove('highlight');
+//     return;
+//   } else if (window.innerWidth > 960 && scrollPosition < 1400) {
+//     aboutMenu.classList.add('highlight');
+//     homeMenu.classList.remove('highlight');
+//     servicesMenu.classList.remove('highlight');
+//     return;
+//   } else if (window.innerWidth > 960 && scrollPosition < 2345) {
+//     servicesMenu.classList.add('highlight');
+//     aboutMenu.classList.remove('highlight');
+//     homeMenu.classList.remove('highlight');
+//     return;
+//   };
 
-  if ((element && window.innerWidth < 960 && scrollPosition < 600) || element) {
-    element.classList.remove('highlight');
-  }
-};
+//   if ((element && window.innerWidth < 960 && scrollPosition < 600) || element) {
+//     element.classList.remove('highlight');
+//   }
+// };
 
-window.addEventListener('scroll', highlightMenu);
-window.addEventListener('click', highlightMenu);
+// window.addEventListener('scroll', highlightMenu);
+// window.addEventListener('click', highlightMenu);
